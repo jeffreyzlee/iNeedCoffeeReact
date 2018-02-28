7971bb94727f6ddddadc05c7ea7c691b517b9462
Brian Lyu <brianlyu96@gmail.com>
2018-02-28 00:53:48 -0800

    finished landing page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,22 +1,13 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
-import LoadingDots from './LoadingDots';
-
-const Header = ({loading}) => {
+import './header.css';
+const Header = () => {
   return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <Link to="/courses" activeClassName="active">Courses</Link>
-      {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
-      {loading && <LoadingDots interval={100} dots={20}/>}
-    </nav>
+    <div className = "headerBanner">
+      <p>iNeedCoffee!</p>
+    </div>
   );
 };
 
-Header.propTypes = {
-  loading: PropTypes.bool.isRequired
-};
 
 export default Header;
