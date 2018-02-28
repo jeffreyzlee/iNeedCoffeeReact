59bb8eedcd161afbf6a60f4f17a45cc1afe04cbd
Jeffrey Lee <lee.jeffrey0714@gmail.com>
2018-02-28 00:41:07 -0800

    Added basic form

--- src/components/agentReg/AgentReg.js
+++ src/components/agentReg/AgentReg.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import {Link} from 'react-router';
+import TextInput from '../common/TextInput';
 
 class AgentReg extends React.Component {
   render() {
     return (
-        <div></div>
+        <form>
+            <h1>Agent Registration</h1>
+            <TextInput
+            name="fullName"
+            label="Full Name"
+            />
+            
+            <TextInput
+            name="email"
+            label="Email"
+            />
+
+            <TextInput
+            name="phone"
+            label="Work Phone"
+            />
+
+            <TextInput
+            name="agency"
+            label="Agency"
+            />
+
+            <TextInput
+            name="salary"
+            label="Desired Compensation"
+            />
+
+            <TextInput
+            name="web"
+            label="Personal Website"
+            />
+
+            <input
+            type="submit"
+            className="btn btn-primary"
+            />
+        </form>
     );
   }
 }
