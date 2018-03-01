a01f34603745a775f179230cb7e0f86a52129d2b
Jeffrey Lee <lee.jeffrey0714@gmail.com>
2018-02-28 22:28:38 -0800

    Added local storage functionality

--- src/components/agentReg/AgentReg.js
+++ src/components/agentReg/AgentReg.js
@@ -3,64 +3,104 @@ import {Link} from 'react-router';
 import TextInput from '../common/TextInput';
 import './agentReg.css';
 import { browserHistory } from 'react-router';
+ 
+  class AgentReg extends React.Component {
+      constructor(){
+          super();
+          this.state = {
+              firstName: '',
+              lastName: '',
+              email: '',
+              phone: '',
+              agency: '',
+              salary: '',
+              web: ''
+          };
+          this.onChange = this.onChange.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this);
+      }
 
-
-class AgentReg extends React.Component {
-  connectPage(){
-    browserHistory.push('connect');
-  }
-  render() {
-    return (
-        <div className = "agentReg">
-          <div className = "innerForm">
-          <form>
-              <h1>Agent Registration</h1>
-              
-              <TextInput
-              name="firstName"
-              label="First Name"
-              />
-
-              <TextInput
-              name="lastName"
-              label="Last Name"
-              />
-              
-              <TextInput
-              name="email"
-              label="Email Address"
-              />
-
-              <TextInput
-              name="phone"
-              label="Work Phone"
-              />
-
-              <TextInput
-              name="agency"
-              label="Agency (If Applicable)"
-              />
-
-              <TextInput
-              name="salary"
-              label="Desired Range of Compensation"
-              />
-
-              <TextInput
-              name="web"
-              label="Personal Website/Portfolio (Optional)"
-              />
-
-              <input
-              type="submit"
-              className="btn btn-primary"
-              onClick = {this.connectPage}
-              />
-          </form>
+      connectPage(){
+        browserHistory.push('connect');
+      }
+  
+      onChange(e) {
+          e.preventDefault();
+          const state = this.state;
+          state[e.target.name] = e.target.value;
+          this.setState(state);
+      }
+  
+      handleSubmit(e){
+          e.preventDefault();
+          const state = this.state;
+          localStorage.setItem('state', JSON.stringify(this.state));
+      }
+      
+      render() {
+          const {firstName, lastName, email, phone, agency, salary, web} = this.state;
+          return (
+          <div>
+              <form onSubmit={this.handleSubmit}>
+                  <h1>Agent Registration</h1>
+                  <TextInput
+                  name="firstName"
+                  label="First Name"
+                  value={firstName}
+                  onChange={this.onChange}
+                  /> 
+  
+                  <TextInput
+                  name="lastName"
+                  label="Last Name"
+                  value={lastName}
+                  onChange={this.onChange}
+                  />
+                  
+                  <TextInput
+                  name="email"
+                  label="Email Address"
+                  value={email}
+                  onChange={this.onChange}
+                  />
+  
+                  <TextInput
+                  name="phone"
+                  label="Work Phone"
+                  value={phone}
+                  onChange={this.onChange}
+                  />
+  
+                  <TextInput
+                  name="agency"
+                  label="Agency (If Applicable)"
+                  value={agency}
+                  onChange={this.onChange}
+                  />
+  
+                  <TextInput
+                  name="salary"
+                  label="Desired Range of Compensation"
+                  value={salary}
+                  onChange={this.onChange}
+                  />
+  
+                  <TextInput
+                  name="web"
+                  label="Personal Website/Portfolio (Optional)"
+                  value={web}
+                  onChange={this.onChange}
+                  />
+  
+                  <input
+                  type="submit"
+                  className="btn btn-primary"
+                  />
+              </form>
           </div>
-        </div>
-    );
+      );
+    }
   }
-}
-
-export default AgentReg;
+  
+  export default AgentReg;
+  
