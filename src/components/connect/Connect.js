7f303448a7c0e7e9ec74a85fc3eb311f895e2395
Jeffrey Lee <lee.jeffrey0714@gmail.com>
2018-03-01 16:05:47 -0800

    Added local storage functionality

--- src/components/connect/Connect.js
+++ src/components/connect/Connect.js
@@ -17,6 +17,19 @@ class Connect extends React.Component {
       chat.value = "";
     }
   }
+
+  getAgentInfo(field){
+    const agent = localStorage.getItem('agentState');
+    let agentInfo = JSON.parse(agent);
+    return agentInfo[field];
+  }
+
+  getOwnerInfo(field){
+    const owner = localStorage.getItem('ownerState');
+    let ownerInfo = JSON.parse(owner);
+    return ownerInfo[field];
+  }
+
   render() {
     return (     
         <div>
@@ -38,13 +51,28 @@ class Connect extends React.Component {
           <div className = "outerRight">
             <div className = "agentCard">
               <h4> AGENT </h4>
-              <p>Name:</p>
-              <p>Phone:</p>
+              <p>Name: {this.getAgentInfo("firstName")} {this.getAgentInfo("lastName")}</p>
+              <p>Email: {this.getAgentInfo("email")}</p>
+              <p>Phone: {this.getAgentInfo("phone")}</p>
+              <p>Agency: {this.getAgentInfo("agency")}</p>
+              <p>Desired Range of Compensation: {this.getAgentInfo("salary")}</p>
+              <p>Personal Website/Portfolio: {this.getAgentInfo("web")}</p>
             </div>
             <div className = "ownerCard">
               <h4> OWNER </h4>
-              <p>Name:</p>
-              <p>Phone:</p>
+              <p>Name: {this.getOwnerInfo("firstName")} {this.getOwnerInfo("lastName")}</p>
+              <p>Email: {this.getOwnerInfo("email")}</p>
+              <p>Work Phone: {this.getOwnerInfo("phone")}</p>
+              <p>Most Recent Employer: {this.getOwnerInfo("work")}</p>
+              <p>Coffee Shop Name: {this.getOwnerInfo("company")}</p>
+              <p>Location 1: {this.getOwnerInfo("l1")}</p>
+              <p>Location 2: {this.getOwnerInfo("l2")}</p>
+              <p>Location 3: {this.getOwnerInfo("l3")}</p>
+              <p>Preferred Rent: {this.getOwnerInfo("r1")}</p>
+              <p>Max Affordable Rent: {this.getOwnerInfo("r2")}</p>
+              <p>Population Density: {this.getOwnerInfo("p1")}</p>
+              <p>Preferred Proximity: {this.getOwnerInfo("u1")}</p>
+              <p>Other Notes: {this.getOwnerInfo("n1")}</p>
             </div>
           </div>
         </div>
