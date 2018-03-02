568f95cd0de172a7faa24023d64cfb7fcd14a61f
Brian Lyu <brianlyu96@gmail.com>
2018-03-01 16:44:50 -0800

    nice

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {Link, browserHistory} from 'react-router';
+import {Link} from 'react-router';
+import { browserHistory } from 'react-router';
 import './landing.css';
 
 class LandingPage extends React.Component {
-  ownerReg(){
-    browserHistory.push('ownerRegistration');
-  }
   agentReg(){
     browserHistory.push('agentRegistration');
+  }
+  ownerReg(){
+    browserHistory.push('ownerRegistration');
   }
   render() {
     return (
       <div className="landing">
         <div>
-            <button onClick={this.agentReg} className="button2">Agents Registration</button>
+          <button onClick={this.ownerReg} className="button">Owners Registration</button>
         </div>
         <div>
-          <button onClick={this.ownerReg} className="button">Owners Registration</button>
+            <button onClick={this.agentReg} className="button">Agents Registration</button>
         </div>
       </div>
     );
   }
 }
-
 
 export default LandingPage;
